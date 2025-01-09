0c19c9222436c2cf31dbe2e31e36b0f97cc3a887
Xyberonzyx7 <advance.houdavid@outlook.com>
2025-01-08 20:53:46 -0800

    disable auto-comment continuation

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable auto-comment continuation
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
+  command = "setlocal formatoptions-=c formatoptions-=r formatoptions-=o"
+})
+
