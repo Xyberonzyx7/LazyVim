8bae1a48ad0787e57cd26707145e035d6971b2eb
Xyberonzyx7 <advance.houdavid@outlook.com>
2025-01-08 20:30:06 -0800

    add 'jk' as the escape key

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Function to set key mappings
+local map = vim.api.nvim_set_keymap
+
+-- Use 'jk' to exit insert mode
+map("i", "jk", "<Esc>", { noremap = true, silent = true })
+
