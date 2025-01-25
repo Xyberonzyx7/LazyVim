912a710679c1645f0e0856b82866fa09917d5af1
Xyberonzyx7 <advance.houdavid@outlook.com>
2025-01-25 00:56:31 -0800

    add: suppress markdown warnings

--- init.lua
+++ init.lua
@@ -1,2 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- suppress markdown warnings
+require("lint").linters_by_ft = {
+  markdown = {
+    "markdownlint",
+  },
+}
+
+require("lint.linters.markdownlint").config = {
+  default = {
+    MD013 = false,
+  },
+}
