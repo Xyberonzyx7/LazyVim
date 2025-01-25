867f1bbe24191799dc6e0d557e2885e7869642c5
Xyberonzyx7 <advance.houdavid@outlook.com>
2025-01-25 12:37:02 -0800

    add: open images and pdfs with microsoft-edge

--- init.lua
+++ init.lua
@@ -13,3 +13,24 @@ require("lint.linters.markdownlint").config = {
     MD013 = false,
   },
 }
+
+-- open pdf and imges with edge
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = { "*.pdf", "*.png", "*.jpg", "*.jpeg", "*.gif" },
+  callback = function()
+    local file_path = vim.fn.expand("%:p")
+    -- Check if the file is the correct type before executing
+    if
+      file_path:match("%.pdf$")
+      or file_path:match("%.png$")
+      or file_path:match("%.jpg$")
+      or file_path:match("%.jpeg$")
+      or file_path:match("%.gif$")
+    then
+      os.execute(
+        'start "" "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe" ' .. vim.fn.shellescape(file_path)
+      )
+      vim.cmd("bdelete!")
+    end
+  end,
+})
