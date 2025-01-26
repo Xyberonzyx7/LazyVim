a450ac9fe54ebbc8ad94ede0bb4784dc6576629f
Xyberonzyx7 <advance.houdavid@outlook.com>
2025-01-25 22:46:14 -0800

    add: nord theme transparent,  add: (commented) tokyonight transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,44 @@
 return {
-    { "shaunsingh/nord.nvim" },
+  { "shaunsingh/nord.nvim" },
 
-    {
-        "LazyVim/LazyVim",
-        opts = {
-            colorscheme = "nord",
-        },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nord",
     },
+  },
+
+  -- Configure transparency for Nord
+  {
+    "shaunsingh/nord.nvim",
+    config = function()
+      -- Set the options
+      vim.g.nord_disable_background = true
+
+      -- Apply the colorscheme
+      vim.cmd([[colorscheme nord]])
+
+      -- Ensure transparency is set
+      vim.cmd([[highlight Normal guibg=NONE ctermbg=NONE]])
+      vim.cmd([[highlight NonText guibg=NONE ctermbg=NONE]])
+    end,
+  },
 }
 
+-- -- tokyonight colorscheme
+-- return {
+--   {
+--     -- Add other plugins here
+--     "folke/tokyonight.nvim",
+--     config = function()
+--       require("tokyonight").setup({
+--         transparent = true,
+--         styles = {
+--           sidebars = "transparent",
+--           floats = "transparent",
+--         },
+--       })
+--       vim.cmd([[colorscheme tokyonight]])
+--     end,
+--   },
+-- }
